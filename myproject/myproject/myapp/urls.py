220b489e7839dced57aeb9de31eba8fb09ef6eac
Angela Busheska <64707998+Angelaangie-ai@users.noreply.github.com>
2023-11-23 01:28:56 -0500

    Delete myproject directory

--- myproject/myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('profile/', views.profile_view, name='profile'),
-    path('', views.index_view, name='index'),
-    path('compare/', views.compare_view, name='compare'),
-]
