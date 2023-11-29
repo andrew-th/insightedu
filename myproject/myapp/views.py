bac09087cfc83ae2f14278eee8c215ad19432d5d
busheska <busheska@lafayette.edu>
2023-11-29 17:37:11 -0500

    Update views.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Enrollment
-from .models import District  # Import the District model
+#from .models import District  # Import the District model
 
 def profile_view(request):
     #districts = District.objects.all()  # Query all district records from the database
     data = Enrollment.objects.all() #Fetch all the records from the Table
-    print(districts)  
+    #print(districts)  
     return render(request, 'myapp/profile.html', {'districts': districts, 'data': data})  # Pass the district data to the template
 
 def index_view(request):
