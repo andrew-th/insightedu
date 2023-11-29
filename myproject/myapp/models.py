72f750a2a375ea8afdebc4573aab6c68b70b3c7f
busheska <busheska@lafayette.edu>
2023-11-29 12:57:39 -0500

    Entering the Enrollment Table

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Table1(models.Model):
-    field1 = models.IntegerField()
+class Enrollment(models.Model):
+    number_of_students = models.IntegerField()
+    number_of_low_income_students = models.IntegerField()
+    year = models.IntegerField()
+    percentage_of_low_income_students = models.IntegerField()
+    school_number = models.IntegerField()
+
 
 class District(models.Model):
     district_name = models.CharField(max_length=255, db_column='district_name')
