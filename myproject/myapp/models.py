e8c4a4e5c1a7417505254a49a2b39585b9d04209
busheska <busheska@lafayette.edu>
2023-11-29 13:35:56 -0500

    Add School Model

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+#Model for Enrollment Table
 class Enrollment(models.Model):
     number_of_students = models.IntegerField()
     number_of_low_income_students = models.IntegerField()
     year = models.IntegerField()
+    school_number = models.IntegerField(primary_key=True)
+
+#Model for School Table
+class School (models.Model):
+    AUN = models.IntegerField()
+    school_name = models.CharField(max_length=255)
     school_number = models.IntegerField(primary_key=True)
 
 
